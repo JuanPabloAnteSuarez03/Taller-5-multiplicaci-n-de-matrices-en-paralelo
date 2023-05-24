f385cf20aa9a31f3a1fa724b87089ad9c9d65e3e
JuanPabloAnte <jp.ant@hotmail.com>
2023-05-24 00:06:00 -0500

    package.scala

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,5 +1,49 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+import common._
+import scala.util.Random
+package objectMatrices {
+  val random = new Random ()
 
-def msg = "I was compiled by Scala 3. :)"
+  type Matriz = Vector[Vector[Int]]
+  
+  def matrizAlAzar(long:Int, vals:Int): Matriz = {
+    val v = Vector.fill(long,long){random.nextInt(vals)}
+    v
+  }
+
+  def transpuesta(m:Matriz):Matriz = {
+    val l=m.length
+    Vector.tabulate(l,l)((i,j)=>m(j)(i))
+  }
+
+  def prodPunto(v1:Vector[Int],v2:Vector[Int]):Int ={
+    (v1 zip v2).map({case(i,j)=>(i∗j)}).sum
+  }
+  // Ejercicio 1.1.1
+  def multMatriz(m1:Matriz,m2:Matriz):Matriz = {
+  . . . }
+  // Ejercicio 1.1.2
+  def multMatrizPar(m1:Matriz,m2:Matriz):Matriz = {
+  . . . }
+  // Ejercicio 1.2.1
+  def subMatriz(m:Matriz, i:Int, j:Int, l:Int):Matriz = {
+  . . .
+  }
+  // Ejercicio 1.2.2
+  def sumMatriz(m1:Matriz, m2:Matriz): Matriz = {
+  . . .
+  }
+  // Ejercicio 1.2.3
+  def multMatrizRec(m1:Matriz, m2:Matriz): Matriz = {
+  . . . }
+  // Ejercicio 1.2.4
+  def multMatrizRecPar(m1:Matriz, m2:Matriz): Matriz = {
+  . . . }
+  // Ejercicio 1.3.1
+  def restaMatriz(m1:Matriz, m2:Matriz): Matriz = {
+  . . . }
+  // Ejercicio 1.3.2
+  def multStrassen(m1:Matriz, m2:Matriz): Matriz = {
+  . . . }
+  // Ejercicio 1.3.3
+  def multStrassenPar(m1:Matriz, m2:Matriz): Matriz = {
+  . . . }
